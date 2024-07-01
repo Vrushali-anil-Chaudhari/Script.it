662c4cd6bf9e03c22784b44e4633e41c6ddccbdc
vrushali <vchaudhari20comp@student.mes.ac.in>
2024-07-01 20:01:49 +0530

    changes in celery task

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -11,12 +11,14 @@ import traceback
 @shared_task(bind=True)
 def upload_truffle_pig(self, index_id):
     try:
-        data_folder = os.path.join(settings.BASE_DIR, 'data')
+        print("in upload to truffle")
+        data_folder = os.path.join(settings.BASE_DIR, 'data', str(index_id))
         client = Trufflepig(settings.TRUFFLE_PIG_KEY)
         print(index_id)
         index = client.get_index(str(index_id))
         print("index",index)
         dir_list = os.listdir(data_folder)
+        print(dir_list)
         total_files = len(dir_list)
         files_upload_response = []
 
@@ -29,11 +31,18 @@ def upload_truffle_pig(self, index_id):
             print(upload_response)
             # Update task progress
             progress = int((i + 1) / total_files * 100)
+            print("PROGRESSSSSSSS",progress)
+            # if progress != 100:
             self.update_state(state='PROGRESS', meta={'progress': progress})
+            # elif progress >= 99:
+            #     self.update_state(state='PROGRESS', meta={'progress': 99})
 
-        return {'status': 'success', 'files_upload_response': files_upload_response}
+        if progress >= 85:
+              self.update_state(state='SUCCESS', meta={'progress': 99})
+
+        return {'status': 'PROGRESS', 'files_upload_response': files_upload_response,'index': index_id}
    
     except Exception as ex: 
         print(ex)
-        self.update_state( state=states.FAILURE, meta={ 'exc_type': type(ex).__name__, 'exc_message': traceback.format_exc().split('\n') }) 
-        return {'status': 'success', 'files_upload_response': str(ex)}
+        self.update_state( state='FAILURE', meta={ 'exc_type': type(ex).__name__, 'exc_message': traceback.format_exc().split('\n') }) 
+        return {'status': 'FAILURE', 'files_upload_response': str(ex)}
